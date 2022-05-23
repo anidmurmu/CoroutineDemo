3a7f27ae986490bea5697d111cce7f64da5f13dc
Anid Murmu <anid.murmu@healthify.co>
2022-05-23 12:24:35 +0530

    lifecycle and Globalscope demo added

--- app/src/main/java/com/example/coroutinedemo/ui/user/MainActivity.kt
+++ app/src/main/java/com/example/coroutinedemo/ui/user/MainActivity.kt
@@ -2,14 +2,26 @@ package com.example.coroutinedemo.ui.user
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.lifecycle.LifecycleCoroutineScope
+import androidx.lifecycle.lifecycleScope
 import com.example.coroutinedemo.R
+import com.example.coroutinedemo.domain.usecase.GetFirstNameUseCase
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private val viewModel: UserViewModel by viewModels()
+
+    @Inject
+    lateinit var getFirstNameUseCase: GetFirstNameUseCase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +33,26 @@ class MainActivity : AppCompatActivity() {
         //viewModel.getListOfUserDetailsSequentially()
         //viewModel.getListOfUserDetailsInParallel()
         //viewModel.cancelCoroutineInWhileExecuting()
-        viewModel.getStreamOfData()
+        //viewModel.getStreamOfData()
+
+        /*GlobalScope.launch(Dispatchers.IO) {
+            val firstName = getFirstNameUseCase.getFirstName("001")
+
+            //Toast.makeText(this@MainActivity, firstName, Toast.LENGTH_SHORT).show()
+
+            withContext(Dispatchers.Main) {
+                Toast.makeText(this@MainActivity, firstName, Toast.LENGTH_SHORT).show()
+            }
+        }*/
+
+        /*lifecycleScope.launch(Dispatchers.IO) {
+            val firstName = getFirstNameUseCase.getFirstName("001")
+
+            //Toast.makeText(this@MainActivity, firstName, Toast.LENGTH_SHORT).show()
+
+            withContext(Dispatchers.Main) {
+                Toast.makeText(this@MainActivity, firstName, Toast.LENGTH_SHORT).show()
+            }
+        }*/
     }
 }
